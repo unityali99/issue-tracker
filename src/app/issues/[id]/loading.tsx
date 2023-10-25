895cbec5b288e6813906339f866d0dee390351fb
Ali Ahmadi <mrkiller1383@gmail.com>
2023-10-25 21:49:58 +0330

    Edited the loading page of issues/[id] route

--- src/app/issues/[id]/loading.tsx
+++ src/app/issues/[id]/loading.tsx
@@ -1,4 +1,5 @@
 import LoadingTheme from "@/components/LoadingTheme";
+import { Card } from "@radix-ui/themes";
 import React from "react";
 import Skeleton from "react-loading-skeleton";
 
@@ -6,7 +7,10 @@ function loading() {
   return (
     <div className="m-10 w-4/12">
       <LoadingTheme>
-        <Skeleton count={6} />
+        <Skeleton count={4} />
+        <Card className="w-3/5">
+          <Skeleton count={3} />
+        </Card>
       </LoadingTheme>
     </div>
   );
